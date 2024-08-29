fc7772d5a63095b21379c4c4ba5c8ee82cec53d4
Dinesh kumar Menapati <113772225+DineshKumarMenapati@users.noreply.github.com>
2024-08-30 00:06:50 +0530

    Update Reverse_Words_in_a_String.cpp

--- Reverse_Words_in_a_String.cpp
+++ Reverse_Words_in_a_String.cpp
@@ -1,1 +1,37 @@
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        stack<string> st;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] != ' ')
+            {
+                string word;
+                while(i  < s.size() && s[i] != ' ')
+                {
+                    word += s[i];
+                    i++;
+                }
+
+                st.push(word);
+            }
+        }
+
+        string ans;
+
+        while(!st.empty())
+        {
+            string topW = st.top();
+            ans = ans + topW + ' ';
+            st.pop();
+        }
+
+        if(ans.size() > 0) ans.pop_back();
+
+        return ans;
+
+    }
+};
